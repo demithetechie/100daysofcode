8aefad3ef5d23258e318255e77c1a131e4a7d4f0
Demi Oshin <demio@Demis-MacBook-Pro.local>
2020-12-01 07:19:45 +0000

    Day 1 Complete

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,1 +1,12 @@
-print("hello world")
+# Day 1 - Python Print Function
+# The function is declared like this
+# print('what to print')
+
+# Use \n to print multiple strings on new lines
+
+print("Hello World\nHello World")
+
+city = input("What city did you grow up in?")
+name = input("What is your name?")
+print("Your band could be called " + city + " " + name)
+
